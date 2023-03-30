710438b4c1552f5265e177b87b6594c0c85ff2e5
OM <orlandomedeiros.junior@gmail.com>
2023-03-30 16:17:06 -0300

    commit

--- Challenge3/main.js
+++ Challenge3/main.js
@@ -1,7 +1,8 @@
 firstChoice = document.querySelector('input[name="area"]');
-butttonFirstChoice = document.querySelector('#escolherArea');
+butttonFirstChoice = document.querySelector('#chooseArea');
 
 butttonFirstChoice.addEventListener("click", function (event) {
+	var secondValue;
 	let message = document.querySelector('#choosedArea')
 	event.preventDefault();
 	const value = firstChoice.value
@@ -9,13 +10,28 @@ butttonFirstChoice.addEventListener("click", function (event) {
 	message.innerHTML = "Você escolheu " + value;
 
 	FirstChoiceFilterToFrontEnd(value)
-	// IndependentChoiceAboutFuture()
+	if (typeof secondValue !== 'undefined') {
+		IndependentChoiceAboutFuture()
+	}
+
 });
+
+function IndependentChoiceAboutFuture() {
+	let phase3Message = document.querySelector('#phase3Message')
+	var x = document.createElement("INPUT");
+	var y = document.createElement("INPUT");
+
+	phase3Message.innerHTML = "Agora que chegou até aqui, qual será seu próximo passo?"
+	x.setAttribute("type", "select");
+	x.setAttribute("placeHolder", "React ou Vue");
+	y.setAttribute("type", "submit");
+	document.body.appendChild(y);
+}
 
 function createInputTextX() {
 	var x = document.createElement("INPUT");
 	x.setAttribute("type", "text");
-	x.setAttribute("name", "secondArea");
+	x.setAttribute("name", "secondNameArea");
 	x.setAttribute("placeHolder", "React ou Vue");
 	document.body.appendChild(x);
 }
@@ -23,25 +39,31 @@ function createInputTextX() {
 function createInputTextY() {
 	var y = document.createElement("INPUT");
 	y.setAttribute("type", "submit");
-	y.setAttribute("id", "secondAreaName");
+	y.setAttribute("id", "secondChooseArea");
 	y.setAttribute("name", "secondArea");
 	document.body.appendChild(y);
 }
 
-function listeningClick(btnToBeHeard) {
-	btnToBeHeard.addEventListener("click", function (event) {
-
+function takeSecondValue() {
+	var buttonSecondChoice = document.querySelector('#secondChooseArea')
+	buttonSecondChoice.addEventListener("click", function (event) {
+		event.preventDefault();
+		let secondMessage = document.querySelector('#secondChoosedArea')
+		var secondChoice = document.querySelector('input[name="secondNameArea"]');
+		const secondValue = secondChoice.value
+		return secondMessage.innerHTML = "Você escolheu " + secondValue;
 	})
 }
 
 function FirstChoiceFilterToFrontEnd(value) {
-	let phaseTwoMessage = document.querySelector('#phaseTwoMessage')
-	let secondMessage = document.querySelector('#secondChoosedArea')
+	var phaseTwoMessage = document.querySelector('#phaseTwoMessage')
 
 	if (value == "Front-End") {
+		phaseTwoMessage.innerHTML = "Já que escolheu Front-End, você prefere React ou Vue?"
+
 		createInputTextX()
 		createInputTextY()
-		phaseTwoMessage.innerHTML = "Já que escolheu Front-End, você prefere React ou Vue?"
+		takeSecondValue()
 
 	} else {
 		if (value == "Back-End") {
@@ -54,17 +76,4 @@ function FirstChoiceFilterToFrontEnd(value) {
 			phaseTwoMessage.innerHTML = "Escolha uma das duas opções disponiveis"
 		}
 	}
-
-}
-
-function IndependentChoiceAboutFuture() {
-	let phase3Message = document.querySelector('#phase3Message')
-	var x = document.createElement("INPUT");
-	var y = document.createElement("INPUT");
-
-	phase3Message.innerHTML = "Agora que chegou até aqui, qual será seu próximo passo?"
-	x.setAttribute("type", "select");
-	x.setAttribute("placeHolder", "React ou Vue");
-	y.setAttribute("type", "submit");
-	document.body.appendChild(y);
 }
