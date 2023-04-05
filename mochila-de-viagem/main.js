a6183a876f2e4ec4fbe939247b15140cb27c9375
OM <orlandomedeiros.junior@gmail.com>
2023-04-05 12:13:46 -0300

    LocalStorage working great, tottaly dinamic.

--- mochila-de-viagem/main.js
+++ mochila-de-viagem/main.js
@@ -1,35 +1,41 @@
 const form = document.getElementById('novoItem')
 const list = document.getElementById('lista')
-const itens = []
+const itens = JSON.parse(localStorage.getItem('itens')) || []
+
+console.log(itens)
+
+itens.forEach((element) => {
+	createElement(element)
+})
 
 form.addEventListener('submit', (event) => {
 	event.preventDefault()
 	const name = event.target.elements['nome']
 	const quantity = event.target.elements['quantidade']
 
-	createElement(name.value, quantity.value)
+	const currentItem = {
+		"name": name.value,
+		"quantity": quantity.value
+	}
+
+	createElement(currentItem)
+
+	itens.push(currentItem)
+
+	localStorage.setItem('itens', JSON.stringify(itens))
 	name.value = ''
 	quantity.value = ''
 })
 
-function createElement(name, quantity) {
+function createElement(item) {
 	const newItem = document.createElement('li')
 	newItem.classList.add('item')
 
 	const numberItem = document.createElement('strong')
-	numberItem.innerHTML = quantity
-
+	numberItem.innerHTML = item.quantity
 	newItem.appendChild(numberItem)
-	newItem.innerHTML += name
+
+	newItem.innerHTML += item.name
 
 	list.appendChild(newItem)
-
-	const currentItem = {
-		"name": name,
-		"quantity": quantity
-	}
-
-	itens.push(currentItem)
-
-	localStorage.setItem('item', JSON.stringify(itens))
 }
